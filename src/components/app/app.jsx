84bea1be375342661a5a21e9d5334ff8a006074e
sergeynagorny <sergey.s.nagorny@gmail.com>
2021-01-06 13:01:14 +0300

    Enzyme. Установка и настройка
    - Добавление обработчика клика для кнопки
    - Обновление снэпшотов

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,14 +1,18 @@
 import React from "react";
 import PropTypes from "prop-types";
-
 import WelcomeScreen from "../welcome-screen/welcome-screen";
 
+
+const welcomeButtonHandler = () => { };
 
 const App = (props) => {
   const {errorsCount} = props;
 
   return (
-    <WelcomeScreen errorsCount={errorsCount} />
+    <WelcomeScreen
+      errorsCount={errorsCount}
+      onWelcomeButtonClick={welcomeButtonHandler}
+    />
   );
 };
 
