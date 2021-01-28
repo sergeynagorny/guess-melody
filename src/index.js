97c1adff75d6362c68f37abbad27e1ee8b3e7513
sergeynagorny <sergey.s.nagorny@gmail.com>
2021-01-28 20:13:01 +0300

    Подключил редьюсеры и `thunk` в `index.js`
    - Установил `redux-devtools-extension`

--- src/index.js
+++ src/index.js
@@ -1,15 +1,34 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import {createStore} from "redux";
+import {createStore, applyMiddleware} from "redux";
 import {Provider} from "react-redux";
-import App from "./components/app/app";
-import {reducer} from "./reducer.js";
+import {composeWithDevTools} from "redux-devtools-extension";
+import thunk from "redux-thunk";
 
+import reducer from "./reducer/reducer.js";
+import {Operation as DataOperation} from "./reducer/data/data.js";
+import {Operation as UserOperation, ActionCreator, AuthorizationStatus} from "./reducer/user/user.js";
+import {createAPI} from "./api.js";
+
+import App from "./components/app/app";
+
+
+const onUnauthorized = () => {
+  store.dispatch(ActionCreator.requireAuthorization(AuthorizationStatus.NO_AUTH));
+};
+
+const api = createAPI(onUnauthorized);
 
 const store = createStore(
     reducer,
-    window.__REDUX_DEVTOOLS_EXTENSION__ ? window.__REDUX_DEVTOOLS_EXTENSION__() : (f) => f
+    composeWithDevTools(
+        applyMiddleware(thunk.withExtraArgument(api))
+    )
+    // window.__REDUX_DEVTOOLS_EXTENSION__ ? window.__REDUX_DEVTOOLS_EXTENSION__() : (f) => f
 );
+
+store.dispatch(DataOperation.loadQuestions());
+store.dispatch(UserOperation.checkAuth());
 
 const init = () => {
   ReactDOM.render(
